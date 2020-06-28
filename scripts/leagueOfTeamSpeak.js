8895dbf22962a9a60883d059990fd31f307785c1
DrWarpMan <DrWarpMan@gmail.com>
2020-06-28 17:00:56 +0200

    changed description

--- scripts/leagueOfTeamSpeak.js
+++ scripts/leagueOfTeamSpeak.js
@@ -1,7 +1,7 @@
 registerPlugin({
     name: "League of TeamSpeak",
     version: "0.0.1",
-    description: "Connect LoL your account with your TS3 account",
+    description: "League of Legends TeamSpeak Integration",
     author: "DrWarpMan <drwarpman@gmail.com>",
     backends: ["ts3"],
     engine: ">= 1.0",
