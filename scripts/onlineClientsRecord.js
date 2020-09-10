a9a2bc9ce589287569d977a196f2a00c7e4a6997
DrWarpMan <DrWarpMan@gmail.com>
2020-09-10 18:05:42 +0200

    version update

--- scripts/onlineClientsRecord.js
+++ scripts/onlineClientsRecord.js
@@ -1,6 +1,6 @@
 registerPlugin({
     name: "Online Clients Record",
-    version: "2.1.0",
+    version: "2.1.1",
     description: "Show the most online clients record in a channel name and population statistics in a channel description!",
     author: "DrWarpMan <drwarpman@gmail.com>",
     backends: ["ts3"],
