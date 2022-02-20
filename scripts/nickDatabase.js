a70092c39793f732f66a5afa4df721238d8ea7ae
DrWarpMan <DrWarpMan@gmail.com>
2022-02-20 17:22:44 +0100

    update

--- scripts/nickDatabase.js
+++ scripts/nickDatabase.js
@@ -1,50 +1,52 @@
-registerPlugin({
-    name: "Nick Database",
-    version: "1.0.0",
-    description: "Script that will save nicknames for each client, to load them later whilst client's are offline!",
-    author: "DrWarpMan <drwarpman@gmail.com>",
-    backends: ["ts3"],
-    engine: ">= 1.0",
-    autorun: true,
-    enableWeb: false,
-    hidden: false,
-    requiredModules: [],
-    voiceCommands: [],
-    vars: [{
-        name: "logEnabled",
-        type: "checkbox",
-        title: "Check to enable detailed logs",
-        default: false
-    }]
-}, (_, config, { name, version, author }) => {
+registerPlugin(
+	{
+		name: "Nick Database",
+		version: "1.0.1",
+		description:
+			"Script that will save nicknames for each client, to load them later whilst client's are offline!",
+		author: "DrWarpMan <drwarpman@gmail.com>",
+		backends: ["ts3"],
+		engine: ">= 1.0",
+		autorun: true,
+		enableWeb: false,
+		hidden: false,
+		requiredModules: [],
+		voiceCommands: [],
+		vars: [
+			{
+				name: "logEnabled",
+				type: "checkbox",
+				title: "Check to enable detailed logs",
+				default: false,
+			},
+		],
+	},
+	(_, config, { name, version, author }) => {
+		const engine = require("engine");
+		const store = require("store");
+		const event = require("event");
 
-    const engine = require("engine");
-    const store = require("store");
-    const event = require("event");
+		const log = msg => !!config.logEnabled && engine.log(msg);
 
-    const { logEnabled } = config;
+		event.on("clientNick", (client, _) => saveNick(client));
+		event.on("clientVisible", ({ client }) => saveNick(client));
 
-    event.on("clientNick", (client, _) => saveNick(client));
-    event.on("clientVisible", ({ client }) => saveNick(client));
+		function getNick(uid) {
+			log(`Grabbing nick of: ${uid}`);
+			return store.get(uid) || false;
+		}
 
-    function getNick(uid) {
-        //logMsg(`Grabbing nick of: ${uid}`);
-        return store.get(uid) || false;
-    };
+		function saveNick(client) {
+			log(`Saving nick of: ${client.uid()} (${client.nick()})`);
+			return store.set(client.uid(), client.nick());
+		}
 
-    function saveNick(client) {
-        //logMsg(`Saving nick of: ${client.uid()} (${client.nick()})`);
-        return store.set(client.uid(), client.nick());
-    };
+		module.exports = {
+			getNick,
+		};
 
-    module.exports = {
-        getNick
-    };
-
-    function logMsg(msg) {
-        return !!logEnabled && engine.log(msg);
-    }
-
-    // SCRIPT LOADED SUCCCESFULLY
-    engine.log(`\n[Script] "${name}" [Version] "${version}" [Author] "${author}"`);
-});
+		engine.log(
+			`\n[LOADED] Script: "${name}" Version: "${version}" Author: "${author}"`
+		);
+	}
+);
