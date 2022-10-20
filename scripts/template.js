6139ed00a819282883c6d7be6c83b6f79d88406a
Šimon Belopotocký <drwarpman@gmail.com>
2022-10-20 18:51:29 +0200

    update

--- scripts/template.js
+++ scripts/template.js
@@ -1,28 +1,35 @@
-registerPlugin({
-    name: "Template",
-    version: "0.1.0",
-    description: "Template description",
-    author: "DrWarpMan <drwarpman@gmail.com>",
-    backends: ["ts3"],
-    engine: ">= 1.0",
-    autorun: false,
-    enableWeb: false,
-    hidden: false,
-    requiredModules: [],
-    voiceCommands: [],
-    vars: [{
-        name: "logEnabled",
-        type: "checkbox",
-        title: "Check to enable detailed logs",
-        default: false
-    }]
-}, (_, config, { name, version, author }) => {
+registerPlugin(
+	{
+		name: "Template",
+		version: "0.1.0",
+		description: "Template description",
+		author: "DrWarpMan <drwarpman@gmail.com>",
+		backends: ["ts3"],
+		engine: ">= 1.0",
+		autorun: false,
+		enableWeb: false,
+		hidden: false,
+		requiredModules: [],
+		voiceCommands: [],
+		vars: [
+			{
+				name: "logEnabled",
+				type: "checkbox",
+				title: "Check to enable detailed logs",
+				default: false,
+			},
+		],
+	},
+	(_, config, { name, version, author }) => {
+		const engine = require("engine");
 
-    const engine = require("engine");
+		// eslint-disable-next-line no-unused-vars
+		const log = msg => !!config.logEnabled && engine.log(msg);
 
-    engine.log(`\n[LOADING] Script: "${name}" Version: "${version}" Author: "${author}"`);
+		// Variables
 
-    const log = msg => !!config.logEnabled && engine.log(msg);
-
-    engine.log(`\n[SUCCESS] Script: "${name}" Version: "${version}" Author: "${author}"`);
-});
+		engine.log(
+			`\n[LOADED] Script: "${name}" Version: "${version}" Author: "${author}"`
+		);
+	}
+);
